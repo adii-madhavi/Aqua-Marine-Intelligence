e58c744b331e0e5e47a221788b4f5b3271744b1c
alpo3n <alpo3n@gmail.com>
2026-02-12 22:46:27 +0530

    Delete aqua_watch directory

--- aqua_watch/ocean_data/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import MarineRecord, SpeciesGrowthCondition
-
-admin.site.register(MarineRecord)
-admin.site.register(SpeciesGrowthCondition)
--- aqua_watch/ocean_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OceanDataConfig(AppConfig):
-    name = 'ocean_data'
